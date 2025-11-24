21949aadd7a44fd9f3be932e8b923a8d1032b59a
michelenero <neromichele0@gmail.com>
2025-11-24 13:14:20 +0100

    Update codice.js

--- arrayJs/codice.js
+++ arrayJs/codice.js
@@ -1,22 +1,10 @@
-//Scrivi un programma in JavaScript che:
-    //Crea un array di numeri (indice 100) con il metodo Math.random() (https://www.w3schools.com/js/js_random.asp) che siano compresi tra -100 e 100.
-    //Calcola la media dei numeri.
-    //Trova il massimo e il minimo.
-    //Crea un vettore contenente solo i numeri maggiori di 50.
-
-
-// Scrivi un programma in JavaScript che:
-// Crea un array di numeri (indice 100) con il metodo Math.random() che siano compresi tra -100 e 100.
-// Calcola la media dei numeri.
-// Trova il massimo e il minimo.
-// Crea un vettore contenente solo i numeri maggiori di 50.
 
 function generaNumeri() {
   const arr = [];
   const arr1 = [];
   let somma = 0;
-  let max = -Infinity;
-  let min = Infinity;
+  let max = 0;
+  let min = 0;
   let n = 0;
 
   for (let i = 0; i < 100; i++) {
@@ -53,5 +41,6 @@ function generaNumeri() {
   }
   document.getElementById("griglia").innerHTML = grigliaHTML;
 }
+
 
 
